aef166c333dccab4b7c7e09662713d673505e6a0
Konstantin <k.kovalev@maygk.ru>
2021-03-08 07:43:27 +0300

    Add file test

--- test.py
+++ test.py
@@ -11,14 +11,14 @@ def yaml_import(file_config):
     return all_device
 
 def connect_to_device(import_file, command):
-    for host in import_file['hosts']:
+    #for host in import_file['hosts']:
         net_connection = Netmiko(
-            host=host['ip'],
-            username=host['username'],
-            password=host['password'],
-            port=host['port'],
-            device_type=host['type'],
-            secret=host['secret']
+            host=import_file['ip'],
+            username=import_file['username'],
+            password=import_file['password'],
+            port=import_file['port'],
+            device_type=import_file['type'],
+            secret=import_file['secret']
         )
         net_connection.enable()
         output = net_connection.send_command(command)
@@ -34,8 +34,9 @@ command ='show run'
 # thread = thread_pool(connect_to_device, all_device, command)
 
 # def thread_pool(function, connect, command):
-with ThreadPoolExecutor(max_workers=3) as executor:
-    result = executor.map(connect_to_device, all_device, repeat(command))
-    for device, output in zip(all_device['hosts'], result):
-        print(device['name'], output)
-        print(device)
+for device in all_device['hosts']:
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        result = executor.map(connect_to_device, device, repeat(command))
+        for device, output in zip(device, result):
+            print(device['name'], output)
+            print(device)
