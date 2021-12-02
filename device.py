eded234e609134a0df40fcd6a198967fb1cde4c4
Konstantin Kovalev <findias@bk.ru>
2021-12-02 13:58:54 +0300

    Add save connection and fix bag

---
+++ device.py
@@ -0,0 +1,13 @@
+sw_c2960_48p_7 = {"device_type": "cisco_ios",
+                  "ip": "172.20.254.7",
+                  "username": "admin",
+                  "password": "Ciscocisco777",
+                  "secret": "Ciscocisco777",
+                  "port": 22
+                 }
+
+
+
+
+
+
